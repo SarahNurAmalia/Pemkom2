1dae427965889d7574c09a1b252724bbe908d3e6
SarahNurAmalia <21.19613@smkn1adw.sch.id>
2026-04-16 09:51:45 +0700

    first commit

---
+++ src/main/java/Karyawan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Karyawan {
+    private String  uidRfid;
+    private String  idKaryawan;
+    private String  namaLengkap;
+    private String  departemen;
+    
+}
