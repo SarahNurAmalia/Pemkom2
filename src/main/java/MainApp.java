d5d94586891ccf80c064efb5ff3c52e40ffb8977
SarahNurAmalia <21.19613@smkn1adw.sch.id>
2026-04-30 10:43:18 +0700

    koneksi database

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -6,20 +6,17 @@
 /**
  *
  * @author ASUS
- */
+*/
+
 public class MainApp {
        public static void main(String[] args) {
-        Karyawan k = new Karyawan();
-        k.setUidRfid("12345678");
-        k.setIdKaryawan("3656");
-        k.setNamaLengkap("Julian"); 
-        k.setDepartemen("Barista"); 
-        
-//        String data = k.toString();
-        System.err.println(k.toString());
-        
-              
-        
+        Karyawan KR = new Karyawan();
+        if(KR instanceof Karyawan){
+            System.err.println("Karyawan");
+        }else {
+            System.err.println("Something else");
+        }// //
+        // //
     }
 }
 
